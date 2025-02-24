a96bd46f712f84cce37978a699fd930b7194281c
JamiNorja <jami.norja@outlook.com>
2025-02-24 13:29:59 +0200

    Login page added -JamiNorja

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,9 +1,61 @@
+'use client';
 
+import React, { useState } from 'react';
+import { Container, TextField, Button, Typography, Box } from '@mui/material';
+import { useRouter } from 'next/navigation';
 
-export default function Home() {
+export default function Login() {
+  const router = useRouter();
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+
+  const handleLogin = () => {
+    if (username === 'fog' && password === 'fog') {
+      router.push('/dashboard');
+    } else {
+      setError('Invalid username or password');
+      setTimeout(() => {
+        setError('');
+      }, 3000);
+    }
+  };
+
   return (
-    <div>
-      hello tähän????
-    </div>
+    <Container maxWidth="xs">
+      <Box
+        display="flex"
+        flexDirection="column"
+        alignItems="center"
+        justifyContent="center"
+        marginTop="20vh"
+        sx={{ border: '3px solid #ccc', padding: 5, borderRadius: 2 }}
+      >
+        <Typography variant="h4" component="h1" gutterBottom sx={{ fontWeight: 'bold', color: 'primary.main' }}>
+          Fog Project Login
+        </Typography>
+        {error && <Typography color="error">{error}</Typography>}
+        <TextField
+          label="Username"
+          variant="outlined"
+          margin="normal"
+          fullWidth
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <TextField
+          label="Password"
+          type="password"
+          variant="outlined"
+          margin="normal"
+          fullWidth
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <Button variant="contained" color="primary" fullWidth sx={{ marginTop: 2 }} onClick={handleLogin} >
+          Login
+        </Button>
+      </Box>
+    </Container>
   );
 }
