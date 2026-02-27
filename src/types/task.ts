e05051d73fd0ea1e46954d514702041e6276caeb
le do <le.do@haaga-helia.fi>
2026-02-27 10:42:38 +0200

    API: Created ActiveTask type

--- src/types/task.ts
+++ src/types/task.ts
@@ -1,5 +1,5 @@
-import { Host } from './host';
-import { Image } from './image';
+import { Host } from "./host";
+import { Image } from "./image";
 
 export interface Task {
   id: number;
@@ -30,4 +30,40 @@ export interface Task {
     name: string;
     description: string;
   };
+}
+
+export interface ActiveTask {
+  id: number;
+  DT_RowId: string;
+  name: string;
+  mainlink: string;
+  checkInTime: string | "No Data";
+  hostID: number;
+  hostLink: string | null;
+  stateID: number;
+  createdTime: string; // ISO format or YYYY-MM-DD HH:mm:ss
+  createdBy: string;
+  isForced: string | boolean;
+  scheduledStartTime: string | "No Data";
+  typeID: number;
+  pct: string; // FOG often sends "0000000000" as a string
+  bpm: string;
+  timeElapsed: string;
+  timeRemaining: string;
+  dataCopied: string;
+  percent: string;
+  dataTotal: string;
+  storagegroupID: number;
+  storagegroupLink: string | null;
+  storagenodeID: number;
+  storagenodeLink: string | null;
+  NFSFailures: string;
+  NFSLastMemberID: number;
+  shutdown: string | boolean;
+  passreset: string;
+  isDebug: number; // Binary 0 or 1
+  imageID: number;
+  imageLink: string | null;
+  wol: string;
+  bypassbitlocker: string;
 }
