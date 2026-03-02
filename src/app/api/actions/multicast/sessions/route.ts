ed6dcb7d8b4037b69e9a7d6259059000726fed86
le do <le.do@haaga-helia.fi>
2026-03-02 10:27:18 +0200

    API: Created new api route api/actions/multicast/sessions to GET all multicast sessions

---
+++ src/app/api/actions/multicast/sessions/route.ts
@@ -0,0 +1,25 @@
+import { fogFetchJson } from "@/lib/fogApi";
+
+export async function GET() {
+  try {
+    const sessions = await fogFetchJson(`/fog/multicastsession`, {
+      method: "GET",
+    });
+
+    return new Response(JSON.stringify(sessions), {
+      status: 200,
+      headers: { "Content-Type": "application/json" },
+    });
+  } catch (error: any) {
+    console.error("Full Error", error);
+    return new Response(
+      JSON.stringify({
+        error: error.message || "Failed to fetch multicast sessions",
+      }),
+      {
+        status: 500,
+        headers: { "Content-Type": "application/json" },
+      },
+    );
+  }
+}
